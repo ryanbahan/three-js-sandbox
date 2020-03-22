b2f186f53319ed5c5b053cebf167c0a8afdd358d
Ryan Bahan <ryanbahan@Ryans-iMac.local>
2020-03-21 18:02:35 -0600

    Add initial sandbox

--- cube.js
+++ cube.js
@@ -1,16 +1,22 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// Degrees, aspect ratio, near clipping, far clipping
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+// Size of render
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
 var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+// Move camera out from 0,0,0 (where the box renders) so box and camera aren't
+// on top of one another
 camera.position.z = 5;
+
 
 var animate = function () {
 	requestAnimationFrame( animate );
